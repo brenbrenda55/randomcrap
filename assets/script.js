fa7bcada14fbe65f04ea639aacef079eaf806aa1
brenbrenda55 <108436173+brenbrenda55@users.noreply.github.com>
2022-08-06 22:45:40 -0500

    outline

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,120 @@
-// time interval
-var counter = 30
-var countdown = function(){
-    console.log(counter);
-    counter--;
-    if (counter === 0){
-        console.log("rip");
-        clearInterval (startCountdown);
-    };
-};
+// variables
+var timerEl = document.getElementById('countdown');
+var mainEl = document.getElementById('main');
+var questionsEl = document.querySelector('questions');
 
-var startCountdown = setInterval(countdown, 3000);
+
+// timer countdown
+var message =
+  'rip game over';
+var words = message.split(' ');
+
+function countdown() {
+    var timeLeft = 15;
+  
+    // TODO: Add a comment describing the functionality of the setInterval() method:
+    var timeInterval = setInterval(function () {
+      // TODO: Add comments describing the functionality of the `if` statement:
+      if (timeLeft > 1) {
+        timerEl.textContent = timeLeft + ' seconds remaining';
+        timeLeft--;
+      } // TODO: Add comments describing the functionality of the `else if` statement:
+      else if (timeLeft === 1) {
+        timerEl.textContent = timeLeft + ' second remaining';
+        timeLeft--;
+      } // TODO: Add comments describing the functionality of the `else` statement:
+      else {
+        timerEl.textContent = '';
+        clearInterval(timeInterval);
+        displayMessage();
+      }
+    }, 1000);
+  }
+
+  // Displays the message one word at a time
+function displayMessage() {
+    var wordCount = 0;
+  
+    // Uses the `setInterval()` method to call a function to be executed every 1000 milliseconds
+    var msgInterval = setInterval(function () {
+      // If there are no more words left in the message
+      if (words[wordCount] === undefined) {
+        // Use `clearInterval()` to stop the timer
+        clearInterval(msgInterval);
+      } else {
+        // Display one word of the message
+        mainEl.textContent = words[wordCount];
+        wordCount++;
+      }
+    }, 1000);
+  }
+  
+  countdown();
+
+
+
+
+//questions
+var questions = [
+    {
+        question: "What song did Doja Cat create all by her self; including costume, music video, and lyrics?",
+        answers : [
+            "moo",
+            " streets",
+            " woman ",
+            " get into it",
+        ],
+        correctAnswerIndex: 1
+    },
+    {
+        question: "Who is jennifer lopez married to as of august 2022?",
+        answers : [
+            "marc anthony",
+            " ben affleck",
+            " cris judd ",
+            " ojani noa",
+        ],
+        correctAnswerIndex: 2
+    },
+    {
+        question: "What is juice wrlds most famous song?",
+        answers : [
+            "all girls are the same",
+            " burn",
+            " lucid dreams ",
+            " lean wit me",
+        ],
+        correctAnswerIndex: 3
+    },
+    {
+        question: "i like to eat, eat, eat, apples and _____?",
+        answers : [
+            "oranges",
+            " kiwis",
+            " pineapples",
+            " bananas",
+        ],
+        correctAnswerIndex: 4
+    },
+    {
+        question: "Whos house did mr. krabs panty raid?",
+        answers : [
+            "mama krabs",
+            " sandy cheeks",
+            " mrs. puff ",
+            " karen plankton",
+        ],
+        correctAnswerIndex: 1
+    },
+];
+
+
+//call back function
+var elemt = document.getElementById('element');
+
+function callback() {
+    alert('Hello');
+}
+
+//add listner
+Element.addEventListner('click', callback);
